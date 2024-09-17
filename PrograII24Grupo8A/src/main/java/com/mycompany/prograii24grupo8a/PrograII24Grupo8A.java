1dae5c20708635babcfd0ffb1930cd6553f30f9c
Dell <Dell@Andy-Mata>
2024-09-16 19:59:58 -0600

    Creacion de Login para nuestro super mercado (Se realizo solamente el Jfream)

---
+++ PrograII24Grupo8A/src/main/java/com/mycompany/prograii24grupo8a/PrograII24Grupo8A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prograii24grupo8a;
+
+/**
+ *
+ * @author Dell
+ */
+public class PrograII24Grupo8A {
+
+    public static void main(String[] args) {
+      
+    }
+}
