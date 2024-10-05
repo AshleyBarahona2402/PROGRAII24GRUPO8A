aa9988b5fa1f6d1bea97c6aa0027f5f8575b1f18
mpg2 <mpg2@DESKTOP-LBB0NQA>
2024-10-04 21:39:11 -0600

    mario paz, creacion de boton eliminar y hacer una lista para poder seleccionar datos

--- PrograII24Grupo8A/src/main/java/Modelo/ClienteDAO.java
+++ PrograII24Grupo8A/src/main/java/Modelo/ClienteDAO.java
@@ -38,6 +38,45 @@ public class ClienteDAO {
         }
 
     }
+public ArrayList<Cliente> ListarCliente() {
+        ArrayList<Cliente> Listacl = new ArrayList();
+        String sql = "SELECT * FROM Clientes";
+        try {
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Cliente cl = new Cliente();
+                cl.setID(rs.getInt("ID"));
+                cl.setNombre(rs.getString("Nombre"));
+                cl.setDireccion(rs.getString("Direccion"));
+                cl.setTelefono(rs.getInt("Telefono"));
+                cl.setNit(rs.getInt("Nit"));
+                Listacl.add(cl);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return Listacl;
+    }
 
+    public boolean EliminarCliente(int ID) {
+        String sql = "DELETE FROM Clientes WHERE ID = ? ";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ID);
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
    
 }
